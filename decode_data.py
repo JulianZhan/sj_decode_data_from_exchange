afbbd69cacb3ca2ae8a01915127b8b1a1f0ca81c
julianzhan <julianzhan6@gmail.com>
2023-12-06 13:12:45 +0800

    use find() to replace interate and check beginning and end of message method

--- decode_data.py
+++ decode_data.py
@@ -1,42 +1,43 @@
+import logging
 from decode_data_utils import *
 from fields_structure_data import *
 
+logging.basicConfig(level=logging.ERROR)
+
 def decode_new_file(file_path):
-    """
-    Decodes the given file path.
+    processed_records = []
 
-    Args:
-        file_path (str): File path.
-
-    Returns:
-        list: List of processed records.
-    """
     with open(file_path, "rb") as file:
         file_data = file.read()
 
-    processed_records = []
-
     start = 0
-    end = 0
+    start_marker = b"x1b"
+    end_marker = b"\r\n"
+    len_end_marker = len(end_marker)
 
-    while end < len(file_data):
-        if is_beginning_of_message(file_data[end : end + 1]):
-            start = end
+    while True:
+        start = file_data.find(start_marker, start)  
+        # if start not found anymore, break
+        if start == -1:
+            break  
 
-        if is_end_of_message(file_data[end : end + 2]):
-            end += 2
-            record_data = file_data[start:end]
-            if len(record_data) <= 20:
-                continue
+        end = file_data.find(end_marker, start) 
+        # if end not found anymore, break
+        if end == -1:
+            break  
 
+        end += len_end_marker
+        record_data = file_data[start:end]
+
+        try:
             processed_record = process_stock_data_dynamic(
                 record_data, sotck_transaction_structure_6
             )
-
             processed_records.append(processed_record)
+        except Exception as e:
+            logging.error(f"Error processing record at {start}-{end}: {e}")
 
-            start = end
-
-        end += 1
+        start = end
 
     return processed_records
+
